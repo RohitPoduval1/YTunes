d0fa63b758109d0cf3fe4ff116de7940ce3b244b
Rohit Poduval <rohit.poduval2@gmail.com>
2026-05-31 18:39:01 -0500

    Unify the interface via arguments

--- README.md
+++ README.md
@@ -3,27 +3,21 @@
 YTag is a command-line application to assign tags to songs in a YouTube playlist and play subsets of songs based on those tags.
 
 ## Usage
-1. Create your CSV with `create_csv_from_playlist.py`
-Given a YouTube playlist URL (first command-line arg), it will write the following contents to the specified CSV file (second arg):
-- The title of the video
-- The URL of the video
-- Empty tags
-
+1. Create your CSV
 ```bash
-python "https://youtube.com/playlist\?list=123ABC456DEF" "playlist_name.csv"
+python ytag.py [-c CREATE] youtube_url [-o OUTPUT] classical.csv
+```
+The created CSV serves as the backend for YTag to operate on with tagging
+
+2. Once you have the CSV file, you can begin tagging
+```bash
+python ytag.py [-e EDIT] classical.csv
 ```
 
-2. Once you have the CSV file, you can begin tagging using `ytag.py`!
+3. Finally, play a playlist, optionally with a tag
 ```bash
-python ytag.py "playlist_name.csv"
+python ytag.py [-p PLAYLIST] classical.csv [-t TAG] "bach"
 ```
-
-3. Finally, play a given playlist using `play_playlist.py` and a tag
-```bash
-python play_playlist "playlist_name.csv" "your_tag_here"
-```
-
-> NOTE: Multi-tag support is in the backlog
 
 ## Background 
 I primarily listen to music via YouTube (no, I do not use Spotify) due to the variety of classical music recordings available (e.g., artists other than Hilary Hahn or Itzhak Perlman).
--- ytag.py
+++ ytag.py
@@ -1,168 +1,106 @@
+import argparse
 import csv
+import re
 import sys
-from dataclasses import dataclass, field
+import subprocess
+import yt_dlp
 
-from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, SelectionList, Input
-from textual.binding import Binding
-
-
-@dataclass
-class Song:
-    id: str
-    title: str
-    tags: set = field(default_factory=set)
-
-    @property
-    def display_str(self) -> str:
-        return f"{self.title.ljust(110)} │ {self.csv_formatted_tags}"
+from tag_app import YTag
 
 
-    @property
-    def csv_formatted_tags(self) -> str:
-        return ', '.join(sorted(self.tags))
+def play_playlist(playlist_path, tag_to_play) -> None:
+    ids = []
+    with open(playlist_path, mode="r") as f:
+        csv_reader = csv.reader(f)
+        for (_, id, tags) in csv_reader:
+            if not tag_to_play:
+                ids.append(id)
+                continue
+
+            if tag_to_play in tags:
+                ids.append(id)
+
+    urls = [f"https://youtu.be/{id}" for id in ids]
+    command = "mpv --no-video --script-opts=ytdl_hook-ytdl_path=yt-dlp --ytdl-format=bestaudio".split(" ")
+    command.extend(urls)
+    subprocess.run(command)
 
 
-class YTag(App):
-    """A Textual app to tag songs in a playlist"""
+def create_csv_from_playlist(playlist_url, csv_filename):
+    ydl_opts = {
+        'extract_flat': True, 
+        'quiet': True
+    }
 
-    BINDINGS = [
-        Binding("j", "move_down", "Down", show=False),
-        Binding("k", "move_up", "Up", show=False),
-        Binding("t", "tag_selected_songs", "Tag Selected"),
-        Binding("escape", "cancel_action", "Cancel"),
-        Binding("backspace", "clear_all_tags", "Clear All Tags"),
-        Binding("q", "quit", "Quit"),
-    ]
+    num_valid_songs = 0
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        print("Fetching playlist data. This may take a moment...")
+        match = re.match(r"((?:https://)?.+/)(.+)", playlist_url)
+        if not match:
+            raise Exception("Regex failed")
+        playlist_id = match.group(2)
+        playlist_id = playlist_id.replace("\\", "")
+        cleaned_playlist_url = match.group(1) + playlist_id
 
-    def __init__(self, csv_path) -> None:
-        super().__init__()
-        self.csv_path = csv_path
+        info_dict = ydl.extract_info(cleaned_playlist_url, download=False)
 
-        with open(self.csv_path, "r", encoding="UTF-8") as f:
-            csv_reader = csv.reader(f)
-            raw_playlist = [tuple(row) for row in csv_reader]
-        
-        self.playlist = {}
-        for (title, id, tags_string) in raw_playlist:
-            parsed_tags = [t.strip() for t in tags_string.split(",") if t.strip()]
-            self.playlist[id] = Song(title=title, id=id, tags=set(parsed_tags))
+        entries = info_dict.get('entries', [])
 
+        # Open a new CSV file to write the data
+        with open(csv_filename, mode='w+', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
 
-    def compose(self) -> ComposeResult:
-        yield Header()
+            # Loop through the playlist and write each video's title and URL
+            for entry in entries:
+                # yt-dlp flat extraction usually returns just the url, title, and id
+                url = entry.get('url')
+                title = entry.get('title')
+                if re.match(r"\[\w+ video\]", title):
+                    continue
 
-        formatted_playlist = [
-            # Textual needs tuples formatted as (Prompt, Value)
-            (song.display_str, song.id)
-            for song in self.playlist.values()
-        ]
-        yield SelectionList[str](*formatted_playlist)
+                writer.writerow([title.strip(), url, ""])
+                num_valid_songs += 1
 
-        tag_input = Input(placeholder="Enter tags (comma separated)...")
-        tag_input.styles.dock = "bottom"
-        self._hide_input_widget(tag_input)
-        yield tag_input
-
-        yield Footer()
+    print(f"✅ {num_valid_songs} videos saved to {csv_filename}!")
+    if num_valid_songs != len(entries):
+        print(f"❌ {len(entries) - num_valid_songs} songs in the playlist are invalid")
 
 
-    def action_move_down(self) -> None:
-        """Move the cursor one down"""
-        self.query_one(SelectionList).action_cursor_down()
+def create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="YTag",
+        description="Tag songs in YouTube playlists and play songs based on those tags",
+    )
+    parser.add_argument("-c", "--create", help="Create YTag compatible playlist from YouTube URL")
+    parser.add_argument("-o", "--output", help="The name of the CSV file to create from the URL")
 
-    def action_move_up(self) -> None:
-        """Move the cursor one up"""
-        self.query_one(SelectionList).action_cursor_up()
+    parser.add_argument("-e", "--edit", help="Edit the tags of the given file")
 
-    
-    def action_clear_all_tags(self) -> None:
-        """Clear all the tags for the selected songs"""
-        # The songs to clear the tags of
-        selected_song_ids = self.query_one(SelectionList).selected
+    parser.add_argument("-p", "--playlist", help="Which playlist to play")
 
-        # Clear in class attribute
-        for song_id in selected_song_ids:
-            self.playlist[song_id].tags = set()
+    # TODO: Multi-tag support
+    parser.add_argument("-t", "--tag", help="Filter tag to play")
 
-        # Clear in CSV
-        self._update_csv_for_playlist()
+    # TODO: Add updating a playlist given the URL while keeping existing tags
 
-        # Refresh the SelectionList UI to show the new tags
-        self._refresh_SelectionListUI()
+    return parser
 
 
-    def action_tag_selected_songs(self) -> None:
-        selected_song_ids = self.query_one(SelectionList).selected
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
 
-        if not selected_song_ids:
-            self.notify("Select at least one song with Spacebar or Enter first!", severity="warning")
-            return
+    if args.create and args.output:
+        create_csv_from_playlist(playlist_url=args.create, csv_filename=args.output)
 
-        # Shift focus to the Input field so user can enter tags
-        tag_input = self.query_one(Input)
-        tag_input.styles.display = "block"
-        tag_input.focus()
+    if args.edit:
+        app = YTag(args.edit)
+        app.run()
 
-
-    def action_cancel_action(self) -> None:
-        """Triggered when the user presses Escape"""
-        tag_input = self.query_one(Input)
-        sel_list = self.query_one(SelectionList)
-        
-        self._hide_input_widget(tag_input)
-        sel_list.focus()
-        sel_list.deselect_all()
-
-
-    def on_input_submitted(self, event: Input.Submitted) -> None:
-        """Triggered automatically when the user presses Enter in the Input widget."""
-        new_tags: str = event.value
-        parsed_new_tags = set([t.strip().lower() for t in new_tags.split(",")])
-        
-        tag_input_widget = self.query_one(Input)
-        sel_list_widget = self.query_one(SelectionList)
-        
-        ### Apply Tags to DB ###
-        selected_ids = sel_list_widget.selected
-        # Update internal playlist structure
-        for id in selected_ids:
-            self.playlist[id].tags.update(parsed_new_tags)
-
-        # Update CSV
-        self._update_csv_for_playlist()
-                
-        # Refresh UI to show new tags
-        self._refresh_SelectionListUI()
-        
-        # Cleanup
-        self._hide_input_widget(tag_input_widget)
-        sel_list_widget.deselect_all()
-        sel_list_widget.focus()
-        self.notify(f"Added tags: {new_tags}")
-
-
-    def _update_csv_for_playlist(self) -> None:
-        with open(self.csv_path, mode="w", newline="", encoding="UTF-8") as f:
-            writer = csv.writer(f)
-            for song in self.playlist.values():
-                writer.writerow([song.title, song.id, song.csv_formatted_tags])
-
-    def _refresh_SelectionListUI(self) -> None:
-        sel_list_widget = self.query_one(SelectionList)
-        sel_list_widget.clear_options()
-        formatted_playlist = [
-            (song.display_str, song.id)
-            for song in self.playlist.values()
-        ]
-        sel_list_widget.add_options(formatted_playlist)
-
-
-    def _hide_input_widget(self, input_widget: Input) -> None:
-        input_widget.value = ""
-        input_widget.styles.display = "none"
+    if args.playlist:
+        play_playlist(playlist_path=args.playlist, tag_to_play=args.tag)
 
 
 if __name__ == "__main__":
-    app = YTag(sys.argv[1])
-    app.run()
+    main()
+
